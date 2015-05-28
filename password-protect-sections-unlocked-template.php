fd772eeb61572e92cce8686abfbeeb06f9865dcf
Patrick Jackson <patrick@ivycat.com>
2015-05-28 13:24:21 -0700

    add hidden inputs to provide password post id, and replace underscores with hyphens for html attribute names

--- password-protect-sections-unlocked-template.php
+++ password-protect-sections-unlocked-template.php
@@ -1,12 +1,19 @@
 <?php
 
-/* 
- * This file contains the content inserted into the content, replacing the
- * content enclosed by the gps_password shortcode tags while that content 
- * is password protected.
+/**
+ * Password Protect Sections UNLOCKED Template
+ *  
+ * This file contains markup that wraps the content enclosed by the gps_password
+ * shortcode when the content has been unlocked.  It allows a visitor to relock
+ * the content, and includes styling to help indicate which content was protected.
  * 
- * Once that code is unlocked by entering the password, the contents of this 
- * file will be replaced by the originally enclosed content.
+ * When the protected section is relocked, the contents of the gps_password shortcode
+ * will be governed by the password-protect-sections-locked-template.php file.
+ * 
+ * @author Patrick Jackson <pjackson@goldenpathsolutions.com>
+ * @copyright (c) 2014, Golden Path Solutions, Inc.
+ * @version 1.0.2
+ * @since 0.1.0
  * 
  */
 
@@ -14,9 +21,10 @@
 ?>
 
 <div class="password-protected-section unlocked">
-    <form name="password_protected_section_<?php echo $password_post->ID; ?>" id="password_protected_section_<?php echo $password_post->ID; ?>"action="" method="post">
+    <form name="password-protected-section-<?php echo $password_post->ID; ?>" id="password-protected-section_<?php echo $password_post->ID; ?>" action="" method="post" class="password-protected-section">
         <?php wp_nonce_field( 'relock_protected_section_'.$password_post->ID ); ?>
-        <input type='hidden' name='relock_protected_section' id='relock-protected-section' value='1'/>
+        <input type="hidden" name="password-name" value="<?php echo $password_post->post_title; ?>"/>
+        <input type='hidden' name='relock-protected-section' id='relock-protected-section' value='1'/>
         <a class='relock-link' href="#" onclick="parentNode.submit()" title="click to relock content"><i class="fa fa-unlock"></i></a>
     </form>
         
