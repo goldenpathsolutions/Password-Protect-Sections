ed27e8fb5ca4e9c031e1b332e654d810f6f7f52c
Patrick Jackson <patrick@ivycat.com>
2015-06-09 08:30:42 -0700

    Make class variables private, and add getters and setters for them where appropriate.

--- classes/class-password-authenticator.php
+++ classes/class-password-authenticator.php
@@ -17,11 +17,33 @@
 
 class Password_Authenticator{
     
-    var $password_post;
+    /**
+     * The Password custom post type being authenticated against
+     * 
+     * @access private
+     * @var object
+     * @since 0.2.0
+     */
+    private $password_post;
     
-    var $is_authenticated = false;
+    /**
+     * The state of the last attempt to authenticate: true if passwords matched,
+     * otherwise false
+     * 
+     * @access private
+     * @var boolean
+     * @since 0.2.0
+     */
+    private $is_authenticated = false;
     
-    var $error;
+    /**
+     * Holds the last error message created during authentication if it exists.
+     * 
+     * @access private
+     * @var string
+     * @since 0.2.0
+     */
+    private $error;
     
     /**
      * 
@@ -33,37 +55,92 @@ class Password_Authenticator{
         $this->password_post = $password_post;
     }
     
+    /**
+     * Return the result of the last attempt to authenticate.  Return false by
+     * default (no attempt to authenticate yet)
+     * 
+     * @return boolean True if most recent password given was authenticated, 
+     *                 otherwise false.
+     * @since 0.2.0
+     */
+    public function is_authenticated(){
+        return $this->is_authenticated;
+    }
+    
+    /**
+     * Use to retrieve the message when there is an error.
+     * 
+     * @return string The last error message written. Null if no error.
+     * 
+     * @since 0.2.0
+     */
+    public function get_error(){
+        return $this->error;
+    }
+    
     
     /**
      * 
-     * Is Authenticated
+     * Set Authenticated
      * 
      * Determine whether a password entered matches the password stored for
      * this gps_password object.  Sets $error with error message on failure.
      * Also sets session variables for authenticatd or failed authentication.
      * 
-     * @param string $password              The password entered that is being tested
+     * @param {string | boolean} $password  The password entered that is being tested
+     *                                      If false, set to deauthenticated
      * @return boolean  True if authenticated, otherwise false
      * 
-     * @since 1.0.0
+     * @since 0.2.0
      */
-    public function is_authenticated( $password ){
+    public function set_authenticated( $password ){
         
         $stored_password = get_post_meta( $this->password_post->ID, '_gps_password', true);
         
-        $authenticated = trim($password) === trim($stored_password);
         
-        if ( $authenticated ){
-            $_SESSION['gps_password_' . $this->password_post->ID 
-                    . '_authenticated'] = true;
-            $_SESSION['gps_password_' . $this->password_post->ID 
-                    . '_failed'] = false;
-        } else {
-            $_SESSION['gps_password_' . $this->password_post->ID 
-                    . '_failed'] = true;
+        // if $password is not false, then test $password against $stored_password...
+        if ( false !== $password ){
+        
+            $authenticated = trim($password) === trim($stored_password);
             
-            $this->error = get_post_meta( $this->password_post->ID, 
-                    '_gps_password_failed_message', true);
+            // if password was correct, set session authentication var to true
+            // and make sure any failed password error is unset
+            if ( $authenticated ){
+            
+                $_SESSION['gps_password_' . $this->password_post->ID 
+                        . '_authenticated'] = true;
+
+                unset( $_SESSION['gps_password_' . $this->password_post->ID 
+                        . '_failed']);
+
+            } else {
+                
+                // otherwise, if the password was wrong, make sure any lingering
+                // authentication session var is unset, set the password failed
+                // session var to true, and pull the error message
+                
+                unset($_SESSION['gps_password_' . $this->password_post->ID 
+                        . '_authenticated']);
+
+                $_SESSION['gps_password_' . $this->password_post->ID 
+                        . '_failed'] = true;
+
+                $this->error = get_post_meta( $this->password_post->ID, 
+                        '_gps_password_failed_message', true);
+            }
+        
+        } else {
+            
+            // otherwise expire authentication: remove session variables, and 
+            // set $authenticated to false
+            
+            unset($_SESSION['gps_password_' . $this->password_post->ID 
+                        . '_authenticated']);
+            unset($_SESSION['gps_password_' . $this->password_post->ID 
+                        . '_failed']);
+            
+            $authenticated = false;
+            
         }
         
         return $authenticated;
